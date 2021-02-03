8e3bebd1c075b221f9cfbf3e5e4f254121cf1f66
lgiacalo <lgiacalo@student.42.fr>
2021-02-03 21:42:35 +0100

    log quick_reply

--- src/handleMessage.js
+++ src/handleMessage.js
@@ -23,6 +23,7 @@ function handleMessage(event) {
 
     if (message.text.toLowerCase().match(regex)) sendHowAreYouMessage(senderID);
     else if (message.quick_reply) {
+      console.log('message.quick_reply :>> ', message.quick_reply);
       const msg = message.quick_reply === 'Good' ? '😁 👍' : '😢';
       sendTextMessage(senderID, msg);
     } else sendTextMessage(senderID, message.text);
