d957ea6e3b760621eca61729dbcacace204a5bf6
lgiacalo <lgiacalo@student.42.fr>
2021-02-04 10:06:40 +0100

    good place ;)

--- src/messages.js
+++ src/messages.js
@@ -3,6 +3,27 @@ const { TOKEN_PAGE_FACEBOOK_MESSENGER } = require('../config');
 
 function tryGetInfoWithMid({ recipient: { id }, message: { mid } }) {
   console.log('{id, mid} :>> ', { id, mid });
+  request(
+    {
+      method: 'GET',
+      uri: `https://graph.facebook.com/v9.0/${mid}`,
+      qs: {
+        access_token: TOKEN_PAGE_FACEBOOK_MESSENGER,
+        fields:
+          'message,from,sticker,tags,id,attachments.limit(10){id,image_data,mime_type,name,file_url,video_data}',
+      },
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log('response GETT :>> ', response);
+        console.log('body GETT :>> ', body);
+      } else {
+        console.error('Unable to get !');
+        // console.error(error);
+        console.error(response.body);
+      }
+    },
+  );
 }
 
 function callSendAPI(messageData) {
@@ -24,11 +45,11 @@ function callSendAPI(messageData) {
           messageId,
           recipientId,
         );
-        tryGetInfoWithMid(messageData);
       } else {
         console.error('Unable to send message');
         // console.error(error);
         console.error(response.body);
+        tryGetInfoWithMid(messageData);
       }
     },
   );
