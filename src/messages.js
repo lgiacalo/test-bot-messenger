e4d375792996b11a8cfc6f644929a2eab7013ef3
lgiacalo <lgiacalo@student.42.fr>
2021-02-04 09:31:02 +0100

    edit error

--- src/messages.js
+++ src/messages.js
@@ -22,7 +22,7 @@ function callSendAPI(messageData) {
         );
       } else {
         console.error('Unable to send message');
-        // console.error(response);
+        // console.error(error);
         console.error(response.body);
       }
     },
